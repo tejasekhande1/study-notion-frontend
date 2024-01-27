bc15d82fe646dae282e161559bfa47351847351a
tejasekhande1 <ekhandetejas741@gmail.com>
2024-01-28 01:48:13 +0530

    add learning language section

--- src/app/components/home/learning-language-section/learning-language-section.component.ts
+++ src/app/components/home/learning-language-section/learning-language-section.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HighlighttextComponent } from '../highlighttext/highlighttext.component';
 
 @Component({
   selector: 'app-learning-language-section',
   standalone: true,
-  imports: [],
+  imports: [HighlighttextComponent],
   templateUrl: './learning-language-section.component.html',
   styleUrl: './learning-language-section.component.scss'
 })
